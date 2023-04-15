6843be7ba4139648df9bf2af9af265bf4ac05dce
unknown <jasmina_t05@outlook.com>
2023-04-15 15:38:04 -0700

    corrected errors and completed activity

--- react_a_mole/src/App.js
+++ react_a_mole/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import MoleContainer from './components/MoleContainer';
+
+import { useState } from 'react';
+
+
 import './App.css';
 
 function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = (numHills = 9) => {
+    let hills = [];
+    for (let i = 0; i < numHills; i++) {
+      hills.push(
+        <MoleContainer
+          key={i}
+          setScore={setScore}
+          score={score}
+        />
+      );
+    }
+    return (
+      <div className='mole-hills'>
+        {hills}
+      </div>
+    )
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{score}</p>
+      {createMoleHill()}
+
     </div>
   );
 }
---
+++ react_a_mole/src/components/Empty.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import molehill from '../embedded/molehill.png'
+
+const RAND_DEFAULT = 5000
+const RAND_MINIMUM = 1000
+
+export default function Empty({ setIsShowing }) {
+
+    useEffect(() => {
+        let randMills = Math.ceil(
+            Math.random() * RAND_DEFAULT + RAND_MINIMUM)
+        let timer = setTimeout(() => {
+                setIsShowing(true)
+        }, randMills)
+
+        return () => clearTimeout(timer)
+    });
+
+    return (
+        <div>
+            <img
+                style={{ 'width': '30vw' }}
+                src={molehill}
+            />
+
+        </div>
+    )
+}
